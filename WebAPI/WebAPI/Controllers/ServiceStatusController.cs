4dc49ec4a482661bc1c77ac14a2cb2a48285d9c4
Ville Palo <ville.palo@avanio.com>
2022-12-01 13:24:01 +0200

    Added health probes

---
+++ WebAPI/WebAPI/Controllers/ServiceStatusController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class ServiceStatusController : ControllerBase
+    {
+        [HttpGet("readines")]
+        public IActionResult GetReadiness()
+        {
+            return new OkResult();
+        }
+
+        [HttpGet("liveness")]
+        public IActionResult GetLiveness()
+        {
+            return new OkResult();
+        }
+    }
+}
