4a64fec645138975ead10595692be93010147cda
Ville Palo <ville.palo@avanio.com>
2022-12-02 08:13:24 +0200

    Adde dindex controller

---
+++ WebAPI/WebAPI/Controllers/IndexController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Controller]
+    [Route("/")]
+    public class IndexController : ControllerBase
+    {
+        private readonly ILogger<IndexController> _logger;
+
+        public IndexController(ILogger<IndexController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            _logger.LogInformation("INDEX");
+            return new OkResult();
+        }
+    }
+}
